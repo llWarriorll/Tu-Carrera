4ff4c6e5ed4b0458aa9a8cd05eb007688a47b0bb
Armand <armanmart2017@gmail.com>
2024-05-14 16:44:39 -0600

    Tercer Commit, ya se agrego la carpeta Templates a Users, donde se agrega el html

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 def helloworld(request):
-    return HttpResponse('Hello world')
+    return render (request, 'signup.html')
